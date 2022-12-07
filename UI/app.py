c30f45e7a516b826066ea07bf38e9797c3cd0a47
NeloferArj <neloferarjumand@gmail.com>
2022-12-07 00:11:16 -0600

    addedroutes

--- UI/app.py
+++ UI/app.py
@@ -6,8 +6,6 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for, redirect
 import pickle
 import finalModel
-# from flask_bootstrap import Bootstrap
-
 app = Flask(__name__)
 
 df = pd.read_csv("prelim_dataset.csv")
@@ -23,36 +21,44 @@ CLF = LinearSVC(class_weight={0: 1, 1: 5}).fit(
 def home():
     return render_template('index.html')
 
+# view detector
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    input = request.form.get("fval")
+    input = request.form.get("text")
     predict = TFIDF.transform([input])
     y_pred = CLF.predict(predict)
     return render_template('index.html', prediction_text='Classification Result: {}'.format(y_pred[0]))
 
 
+# view dashboard
 @app.route('/dashboard')
 def dashboard():
     '''
-    For rendering graphs 
+    For rendering graphs
     '''
-    text = request.args['text']
-    source = request.args['source']
-    label = request.args['label']
-    input = "Viewing bashboard"
-    return render_template('dashboard.html', text=text, source=source, label=label)
+    # getting input data from addDataForm endpoint
+    # text = request.args['text']
+    # source = request.args['source']
+    # label = request.args['label']
+    # return render_template('dashboard.html', text=text, source=source, label=label)
+
+    return render_template('dashboard.html')
 
 
+# view description
 @app.route('/description')
 def description():
     '''
     For rendering description 
     '''
     return render_template('description.html')
+
+# view form to allow user to add data
 
 
 @app.route('/addData')
@@ -61,6 +67,8 @@ def addData():
     For adding data 
     '''
     return render_template('addData.html')
+
+# get data entered by user
 
 
 @app.route('/addDataForm', methods=['POST'])
@@ -71,7 +79,10 @@ def addDataForm():
     text = request.form.get("text")
     source = request.form.get("source")
     label = request.form.get("label")
-    return redirect(url_for('dashboard', text=text, source=source, label=label))
+
+    # pass data to dashboard endpoint or  pass data to cli.py
+    return redirect(url_for('dashboard'))
+    # return redirect(url_for('dashboard', text=text, source=source, label=label))
 
 
 if __name__ == "__main__":
