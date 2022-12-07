fc703bcdc9c277cffefb6d89535dd7c3b745c0e9
NeloferArj <neloferarjumand@gmail.com>
2022-12-06 23:39:02 -0600

    changesapp

--- UI/app.py
+++ UI/app.py
@@ -1,7 +1,14 @@
+<<<<<<< Updated upstream
 from flask import Flask, request, render_template
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn.svm import LinearSVC
+=======
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for, redirect
+import pickle
+import finalModel
+>>>>>>> Stashed changes
 # from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -33,8 +40,11 @@ def dashboard():
     '''
     For rendering graphs 
     '''
+    text = request.args['text'] or "Enter data using 'Add Data' tab"
+    source = request.args['source'] or "Enter data using 'Add Data' tab"
+    label = request.args['label'] or "Enter data using 'Add Data' tab"
     input = "Viewing bashboard"
-    return render_template('dashboard.html', text=input)
+    return render_template('dashboard.html', text=text, source=source, label=label)
 
 
 @app.route('/description')
@@ -43,6 +53,25 @@ def description():
     For rendering description 
     '''
     return render_template('description.html')
+
+
+@app.route('/addData')
+def addData():
+    '''
+    For adding data 
+    '''
+    return render_template('addData.html')
+
+
+@app.route('/addDataForm', methods=['POST'])
+def addDataForm():
+    '''
+    For getting data and redirecting
+    '''
+    text = request.form.get("text")
+    source = request.form.get("source")
+    label = request.form.get("label")
+    return redirect(url_for('dashboard', text=text, source=source, label=label))
 
 
 if __name__ == "__main__":
--- cli.py
+++ cli.py
@@ -9,7 +9,8 @@ c = Counter('counter', 'Data counter', ['islamophobic', 'source'])
 df_twitter = pd.read_csv("TwitterData/prelim_dataset.csv")
 df_reddit = pd.read_csv("TwitterData/RedditClean.csv")
 
-t_islamophobic_count = df_twitter.loc[df_twitter["Islamophobic?"] == 1.0].shape[0]
+t_islamophobic_count = df_twitter.loc[df_twitter["Islamophobic?"]
+                                      == 1.0].shape[0]
 t_nonislampphobic_count = df_twitter.loc[df_twitter["Islamophobic?"] == 0.0].shape[0]
 
 r_islamophobic_count = df_reddit.loc[df_reddit["islamophobic"] == 1].shape[0]
@@ -17,6 +18,8 @@ r_nonislampphobic_count = df_reddit.loc[df_reddit["islamophobic"] == 0].shape[0]
 c.labels("yes", "twitter").inc(t_islamophobic_count)
 c.labels("no", "twitter").inc(t_nonislampphobic_count)
 c.labels("yes", "reddit").inc(r_islamophobic_count)
+
+
 c.labels("no", "reddit").inc(r_nonislampphobic_count)
 
 while True:
